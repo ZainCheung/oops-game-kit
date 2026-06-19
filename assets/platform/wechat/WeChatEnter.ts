ce19db761757f68a4618fd0cdad6605d6edd250c
dgflash <dgflash@qq.com>
2026-06-19 13:44:36 +0800

    1

--- assets/platform/wechat/WeChatEnter.ts
+++ assets/platform/wechat/WeChatEnter.ts
@@ -5,12 +5,18 @@ const { ccclass } = _decorator;
 @ccclass('WeChatEnter')
 export class WeChatEnter extends Component {
     async onLoad() {
+        await this.loadBundle('game_main');
         director.loadScene('main');
     }
 
     private loadBundle(name: string): Promise<void> {
         return new Promise((resolve, reject) => {
             assetManager.loadBundle(name, (err: Error | null, data: AssetManager.Bundle) => {
+                if (err) {
+                    console.error(`加载 bundle ${name} 失败`, err);
+                    reject(err);
+                    return;
+                }
                 resolve();
             });
         })
