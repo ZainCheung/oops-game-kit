f6b2e1c9ea2662f75beeca2b7ae7b69f3686f923
dgflash <dgflash@qq.com>
2026-06-19 20:52:06 +0800

    .

--- assets/bundle/game_main/script/game/account/bll/B_Account_Module.ts
+++ assets/bundle/game_main/script/game/account/bll/B_Account_Module.ts
@@ -3,11 +3,12 @@ import { classname } from 'db://oops-framework/module/decorator/ClassNameDecorat
 import { CCEntity } from 'db://oops-framework/module/common/CCEntity';
 import type { Account } from '../Account';
 import { Guide } from '../../guide/Guide';
+import { RedDot } from '../../reddot/RedDot';
 
 /** 账号子模块管理 */
 @classname('B_Account_Module')
 export class B_Account_Module extends CCBusiness<Account> {
     protected init() {
-        this.ent.addChildSingletons<CCEntity>(Guide);
+        this.ent.addChildSingletons<CCEntity>(Guide, RedDot);
     }
 }
