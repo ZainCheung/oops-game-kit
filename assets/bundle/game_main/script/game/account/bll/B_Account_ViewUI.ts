b509a768ecfa8ce52713cc36fabd39465efb5ae2
dgflash <dgflash@qq.com>
2026-06-20 18:23:52 +0800

    .

--- assets/bundle/game_main/script/game/account/bll/B_Account_ViewUI.ts
+++ assets/bundle/game_main/script/game/account/bll/B_Account_ViewUI.ts
@@ -1,9 +1,11 @@
 import type { Node } from 'cc';
 import { CCBusiness } from 'db://oops-framework/module/common/CCBusiness';
+import { classname } from 'db://oops-framework/module/decorator/ClassNameDecorator';
 import type { Account } from '../Account';
 import { VC_Account_Login } from '../view/VC_Account_Login';
 
 /** Account视图管理逻辑 */
+@classname('B_Account_ViewUI')
 export class B_Account_ViewUI extends CCBusiness<Account> {
     /** 打开Login界面 */
     openLogin(): Promise<Node | null> {
