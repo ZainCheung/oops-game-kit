22bdffc7889c5f9bcf7af17c957df34a199edc6f
dgflash <dgflash@qq.com>
2026-06-27 10:47:22 +0800

    .

--- assets/bundle/game_main/script/game/account/bll/B_Account_ViewUI.ts
+++ assets/bundle/game_main/script/game/account/bll/B_Account_ViewUI.ts
@@ -7,16 +7,6 @@ import { V_Account_Authorization } from '../view/V_Account_Authorization';
 /** Account视图管理逻辑 */
 @classname('B_Account_ViewUI')
 export class B_Account_ViewUI extends CCBusiness<Account> {
-    // /** 打开Login界面 */
-    // openLogin(): Promise<Node | null> {
-    //     return this.ent.addUi(VC_Account_Login);
-    // }
-
-    // /** 关闭Login界面 */
-    // removeLogin(): void {
-    //     this.ent.removeUi(VC_Account_Login);
-    // }
-
     /** 打开Authorization界面 */
     openAuthorization(): Promise<Node | null> {
         return this.ent.addUi(V_Account_Authorization);
