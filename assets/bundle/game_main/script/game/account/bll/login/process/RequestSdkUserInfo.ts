3e6c462e572ab56fe582d9b0618892fab75ca71b
dgflash <dgflash@qq.com>
2026-06-27 14:30:56 +0800

    .

--- assets/bundle/game_main/script/game/account/bll/login/process/RequestSdkUserInfo.ts
+++ assets/bundle/game_main/script/game/account/bll/login/process/RequestSdkUserInfo.ts
@@ -41,7 +41,7 @@ export class RequestSdkUserInfo extends LoginProcessBase {
 
     protected async execute() {
         // 0. 命中缓存直接 finish
-        const raw = oops.storage.getJson<IUserInfo | null>('UserInfoCache', null);
+        const raw = oops.storage.getJson<IUserInfo | null>('GameUserInfoCache', null);
         if (raw?.nickName) {
             this.finish(raw);
             return;
@@ -139,7 +139,7 @@ export class RequestSdkUserInfo extends LoginProcessBase {
     private finish(userInfo: IUserInfo, writeCache = false): void {
         gsm.base.sdk.userInfo = userInfo;
         if (writeCache) {
-            oops.storage.set('UserInfoCache', userInfo);
+            oops.storage.set('GameUserInfoCache', userInfo);
         }
         console.log('【登录流程】用户信息:', userInfo);
         this.success();
